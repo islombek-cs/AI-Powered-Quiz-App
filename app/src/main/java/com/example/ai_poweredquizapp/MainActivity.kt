02b656e5a6595693b0dc088b7d9408d82d9a3153
islombek-cs <jekinsma1604@gmail.com>
2025-04-20 23:18:05 +0500

    Implemented bottom navigation, view binding, and UI layout structure

--- app/src/main/java/com/example/ai_poweredquizapp/MainActivity.kt
+++ app/src/main/java/com/example/ai_poweredquizapp/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ai_poweredquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,31 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Inflate layout using ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Handle BottomNavigationView item clicks
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            val itemId = menuItem.itemId
+
+            when (itemId) {
+                R.id.item_home -> {
+
+                }
+                R.id.item_assessment -> {
+
+                }
+                R.id.item_task -> {
+
+                }
+                R.id.item_profile -> {
+
+                }
+            }
+            true
         }
     }
 }
