c18e5f3315c5765d497882adda7db3005dd3539f
islombek-cs <jekinsma1604@gmail.com>
2025-04-20 23:50:07 +0500

    Implemented bottom navigation UI structure with icons and activity_main.xml updates

--- app/src/main/java/com/example/ai_poweredquizapp/MainActivity.kt
+++ app/src/main/java/com/example/ai_poweredquizapp/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ai_poweredquizapp.databinding.ActivityMainBinding
+import com.example.ai_poweredquizapp.fragments.AssessmentFragment
+import com.example.ai_poweredquizapp.fragments.HomeFragment
+import com.example.ai_poweredquizapp.fragments.ProfileFragment
+import com.example.ai_poweredquizapp.fragments.TaskFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,25 +30,65 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        By default (when app opens) show HomeFragment
+        showHomeFragment()
+
         // Handle BottomNavigationView item clicks
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
             val itemId = menuItem.itemId
 
             when (itemId) {
                 R.id.item_home -> {
-
+                    showHomeFragment()
                 }
                 R.id.item_assessment -> {
-
+                    showAssessmentFragment()
                 }
                 R.id.item_task -> {
-
+                    showTaskFragment()
                 }
                 R.id.item_profile -> {
-
+                    showProfileFragment()
                 }
             }
             true
         }
     }
+
+    private fun showHomeFragment() {
+        binding.toolbarTitleTv.text = "Home"
+
+        val homeFragment = HomeFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFL.id, homeFragment, "HomeFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showAssessmentFragment() {
+        binding.toolbarTitleTv.text = "Assessment"
+
+        val assessmentFragment = AssessmentFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFL.id, assessmentFragment, "AssessmentFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showTaskFragment() {
+        binding.toolbarTitleTv.text = "Task"
+
+        val taskFragment = TaskFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFL.id, taskFragment, "TaskFragment")
+        fragmentTransaction.commit()
+    }
+
+    private fun showProfileFragment() {
+        binding.toolbarTitleTv.text = "Profile"
+
+        val profileFragment = ProfileFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFL.id, profileFragment, "ProfileFragment")
+        fragmentTransaction.commit()
+    }
+
 }
